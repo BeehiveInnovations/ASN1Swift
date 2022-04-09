86dd16a132c8258a8cd409aad568b18ee6f82894
Fahad <fahad@guidedways.com>
2022-04-09 04:27:20 +0400

    Added project for xcframework

---
+++ Sources/ASN1Swift/ASN1Swift.h
@@ -0,0 +1,18 @@
+//
+//  ASN1Swift.h
+//  ASN1Swift
+//
+//  Created by Fahad on 09/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASN1Swift.
+FOUNDATION_EXPORT double ASN1SwiftVersionNumber;
+
+//! Project version string for ASN1Swift.
+FOUNDATION_EXPORT const unsigned char ASN1SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASN1Swift/PublicHeader.h>
+
+
